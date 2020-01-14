4733d6d9f31929d1a57dd7da19e039434901827b
DlhSoft Team <Contact@DlhSoft.com>
2020-01-14 17:07:00 +0200

    Vue documentation

--- GanttChartHyperLibraryDocumentation/Documentation/app.js
+++ GanttChartHyperLibraryDocumentation/Documentation/app.js
@@ -12,14 +12,14 @@ angular.module('Documentation', [])
     $scope.selectFile = function (file) {
         $scope.selectedFile = file;
     };
-    var technologies = [{ name: 'JavaScript', title: 'HTML + JavaScript®' }, { name: 'TypeScript', title: 'HTML + TypeScript' }, { name: 'AngularJS', title: 'AngularJS' }, { name: 'Angular', title: 'Angular 8' }, { name: 'React', title: 'React' }];
+    var technologies = [{ name: 'JavaScript', title: 'HTML + JavaScript®' }, { name: 'TypeScript', title: 'HTML + TypeScript' }, { name: 'AngularJS', title: 'AngularJS' }, { name: 'Angular', title: 'Angular 8' }, { name: 'React', title: 'React' }, { name: 'Vue', title: 'Vue' }];
     $scope.technologies = technologies;
     $scope.selectedTechnology = technologies[0];
     $scope.selectTechnology = function (technology) {
         if (technology == $scope.selectedTechnology)
             return;
         $scope.selectedTechnology = technology;
-        if (technology.name == "Angular" || technology.name == "React") {
+        if (technology.name == "Angular" || technology.name == "React" || technology.name == "Vue") {
             var visibleFiles = files.slice();
             visibleFiles.pop();
             $scope.visibleFiles = visibleFiles;
@@ -36,6 +36,8 @@ angular.module('Documentation', [])
         $scope.selectedTechnology = technologies[3];
     else if (initialSelection == 'React')
         $scope.selectedTechnology = technologies[4];
+    else if (initialSelection == 'Vue')
+        $scope.selectedTechnology = technologies[5];
     else
         $scope.selectedFile = initialSelection;
     $scope.isInitializing = true;
--- GanttChartHyperLibraryDocumentation/Documentation/app.ts
+++ GanttChartHyperLibraryDocumentation/Documentation/app.ts
@@ -14,14 +14,14 @@ angular.module('Documentation', [])
         $scope.selectFile = (file) => {
             $scope.selectedFile = file;
         };
-        var technologies = [{ name: 'JavaScript', title: 'HTML + JavaScript®' }, { name: 'TypeScript', title: 'HTML + TypeScript' }, { name: 'AngularJS', title: 'AngularJS' }, { name: 'Angular', title: 'Angular 8' }, { name: 'React', title: 'React' }];
+        var technologies = [{ name: 'JavaScript', title: 'HTML + JavaScript®' }, { name: 'TypeScript', title: 'HTML + TypeScript' }, { name: 'AngularJS', title: 'AngularJS' }, { name: 'Angular', title: 'Angular 8' }, { name: 'React', title: 'React' }, { name: 'Vue', title: 'Vue' }];
         $scope.technologies = technologies;
         $scope.selectedTechnology = technologies[0];
         $scope.selectTechnology = (technology) => {
             if (technology == $scope.selectedTechnology)
                 return;
             $scope.selectedTechnology = technology;
-            if (technology.name == "Angular" || technology.name == "React") {
+            if (technology.name == "Angular" || technology.name == "React" || technology.name == "Vue") {
                 var visibleFiles = files.slice();
                 visibleFiles.pop(); // "More" should not be available
                 $scope.visibleFiles = visibleFiles;
@@ -38,6 +38,8 @@ angular.module('Documentation', [])
             $scope.selectedTechnology = technologies[3];
         else if (initialSelection == 'React')
             $scope.selectedTechnology = technologies[4];
+        else if (initialSelection == 'Vue')
+            $scope.selectedTechnology = technologies[5];
         else
             $scope.selectedFile = initialSelection;
         $scope.isInitializing = true;
